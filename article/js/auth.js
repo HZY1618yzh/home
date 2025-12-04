03f9bc039dec1e421901493a8f62c787cc157fa7
HZY1618yzh <13326945856@sina.cn>
2025-12-04 16:44:06 +0800

    Update Supabase admin password in auth.js

--- article/js/auth.js
+++ article/js/auth.js
@@ -1,7 +1,7 @@
 window.supabaseConfig = {
     url: 'https://gimpssdzcwrdubylyouo.supabase.co',
     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdpbXBzc2R6Y3dyZHVieWx5b3VvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ2NTA3ODYsImV4cCI6MjA4MDIyNjc4Nn0.1kKpKWcTmUjcc5Y_zrrqrTTS7FeoDD5P7yBLMGP-5pE',
-    adminPassword: 'azsxdcfv12345AZSXDCFV!@#$%'
+    adminPassword: 'l11l11l1ll1ll1'
 };
 window.supabase = supabase.createClient(
     window.supabaseConfig.url, 
